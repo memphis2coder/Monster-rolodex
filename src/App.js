2bed38931b44b43f8f79e0d4aaf81d55006705e4
Antonio <memphis2coder@gmail.com>
2020-08-22 02:05:49 -0400

    connect to api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: []
+    };
+  }
+  // lifecycle method... connect to api
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json()) // receive a promise & change data to json
+      .then(users => this.setState({monsters: users}))
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            hello my name is tony
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        {this.state.monsters.map(monster => ( // map over the monsters array 
+            <h1 key={monster.id}>
+              {monster.name}
+            </h1>
+          ))}
+
     </div>
     )
   }
