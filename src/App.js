7101136cbdccc5648fdd157e0c80587066ab1dac
Antonio <memphis2coder@gmail.com>
2020-08-23 00:58:36 -0400

    working on search feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import CardList from './components/cardList/cardList';
+import SearchBar from './components/searchBar/SearchBar';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
 
+    // state only lives in 1 location and is passed down
     this.state = {
       monsters: []
     };
@@ -20,7 +22,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <SearchBar monsters={this.state.monster}/> {/** passing state down as a attribute */} 
+        <CardList monsters={this.state.monsters} /> {/** passing state down as a attribute */} 
         
     </div>
     )
---
+++ src/components/searchBar/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './SearchBar.css';
+
+export default function SearchBar(props) {
+    return (
+        <div className="search-bar">
+            filter: <input type="text"/>
+        </div>
+    )
+}
